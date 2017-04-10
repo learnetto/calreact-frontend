7712a5f38c15e0c5fd882b0f8eaae4966bb56d58
Hrishi Mittal <hrishimittal@gmail.com>
2017-04-10 17:01:17 +0100

    Add test for displaying title

--- src/__tests__/Appointment.test.js
+++ src/__tests__/Appointment.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Appointment from '../components/Appointment';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Router><Appointment /></Router>, div);
+  shallow(<Appointment />);
+});
+
+describe('render', () => {
+	it('should display the appointment title', () => {
+    const appointment = mount(<Router><Appointment appointment={{id:1, title: 'Team standup meeting', appt_time: new Date()}} /></Router>);
+    const title = <h3>Team standup meeting</h3>;
+		expect(appointment.contains(title)).toEqual(true);
+	})
 });
