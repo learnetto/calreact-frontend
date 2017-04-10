dd2e31065fa88b6e5ef8fbcef134037403e376da
Hrishi Mittal <hrishimittal@gmail.com>
2017-04-10 18:38:31 +0100

    Add render test for appointment time

--- src/__tests__/Appointment.test.js
+++ src/__tests__/Appointment.test.js
@@ -14,4 +14,10 @@ describe('render', () => {
     const title = <h3>Team standup meeting</h3>;
 		expect(appointment.contains(title)).toEqual(true);
 	})
+
+	it('should display the appointment time', () => {
+    const appointment = mount(<Router><Appointment appointment={{id:1, title: 'Team standup meeting', appt_time: new Date('04/11/2017, 12:00:00')}} /></Router>);
+    const appt_time = <p>April 11 2017, 12:00:00 pm</p>;
+		expect(appointment.contains(appt_time)).toEqual(true);
+	})
 });
