127e970bc5b82aca79026bbb96e934740a031001
Hrishi Mittal <hrishimittal@gmail.com>
2017-04-11 14:29:02 +0100

    Add validation button test

--- src/__tests__/AppointmentForm.test.js
+++ src/__tests__/AppointmentForm.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import Appointment from '../components/Appointment';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import AppointmentForm from '../components/AppointmentForm';
 
 it('renders without crashing', () => {
   shallow(<AppointmentForm />);
+});
+
+it('toggles form submit button disabled status based on form validity', () => {
+	const wrapper = mount(<Router><AppointmentForm /></Router>);
+	expect(wrapper.find('.submit-button').props().disabled).toEqual(true);
+
+	wrapper.find('[name="title"]')
+				 .simulate('change', {target: {value: 'A valid title', name: 'title'}});
+
+	wrapper.find('.rdtNext span').at(0).simulate('click');
+	wrapper.find('.rdtDay').at(10).simulate('click');
+
+	expect(wrapper.find('.submit-button').props().disabled).toEqual(false);
 });
